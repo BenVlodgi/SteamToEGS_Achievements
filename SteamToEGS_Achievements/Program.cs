35772cfc03fbab29cbb39672cab000b86f6509dd
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2021-05-06 11:28:29 -0400

    Add new Project

---
+++ SteamToEGS_Achievements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamToEGS_Achievements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
